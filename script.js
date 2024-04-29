a19f7d548c2c41f48e6ed265e678894279d5408b
dprac <156108452+dprac@users.noreply.github.com>
2024-04-29 01:54:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function changeContent(contentId) {
   
   // Fetch content based on contentId
   var newContent;
-   if (contentId === 'about') {
+   if (contentId === 'About Me') {
     newContent = "<h2>About Me</h2>" +
                  "<p>This is the content about me. You can write anything you want to introduce yourself here.</p>";
-  } else if (contentId === 'project1') {
+  } else if (contentId === 'Project 1') {
     newContent = "<h2>Project 1</h2>" +
                  "<p>Project 1 is a poem based around the concept of "uncreative writing." Describe your project and its features here.</p>";
-  } else if (contentId === 'project2') {
+  } else if (contentId === 'Project 2') {
     newContent = "<h2>Project 2</h2>" +
                  "<p>This is the content for Project 2. Provide details about your second project here.</p>";
-  } else if (contentId === 'project3') {
+  } else if (contentId === 'Project 3') {
     newContent = "<h2>Project 3</h2>" +
                  "<p>This is the content for Project 3. Describe your third project here.</p>";
   }
